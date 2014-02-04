5253405757348023c92de3bdacd7f2fdf0c36ce3
Glenn K. Lockwood <glock@sdsc.edu>
2014-02-04 14:35:54 -0800

    made clog-generate more robust in light of a formatting change to nodeview

--- clog-generate.sh
+++ clog-generate.sh
@@ -7,10 +7,10 @@ mfdir='/oasis/projects/nsf/use300/glock/machinestate/gordon'
 nodeview=$(which nodeview)
 
 cd $mfdir
-for mf in 201312*.db
+for mf in 201401*.db
 do
   # 2013 12 01 12 00.db
   timestamp=$( sed -e 's#\(....\)\(..\)\(..\)\(..\)\(..\).db#\2/\3/\1 \4:\5#' <<< $mf )
   echo -n -e "$timestamp\t"
-  $nodeview --readdb=$mf --summary --quiet | tail -n2 | awk '{ printf( "%8d", $4/1000 ) } END { printf( "\n" ); }'
+  $nodeview --readdb=$mf --summary --quiet | grep 'Total SUs' | cut -d: -f 2 | awk '{ printf( "%8d", $1/1000 ) } END { printf( "\n" ); }'
 done
