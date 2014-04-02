2ab37a3ca557894159e383b780db6793a572d165
Glenn K. Lockwood <glock@sdsc.edu>
2014-04-01 20:44:42 -0700

    generalized clog plot generator

--- clog-generate.sh
+++ clog-generate.sh
@@ -3,14 +3,17 @@
 #  Use this script to generate Kenneth's queue clog metrics for a month
 #
 
-mfdir="/oasis/projects/nsf/use300/glock/machinestate/$(uname -n | cut -d - -f 1)"
+year=2014
+month=03
+
+mfdir="/oasis/projects/nsf/use300/glock/machinestate/$(uname -n | cut -d - -f 1)/$year-$month"
 nodeview=$(which nodeview)
 
 cd $mfdir
-for mf in 201401*.db
+for mf in ${year}${month}*.db
 do
   # 2013 12 01 12 00.db
   timestamp=$( sed -e 's#\(....\)\(..\)\(..\)\(..\)\(..\).db#\2/\3/\1 \4:\5#' <<< $mf )
   echo -n -e "$timestamp\t"
-  $nodeview --readdb=$mf --summary --quiet | grep 'Total SUs' | cut -d: -f 2 | awk '{ printf( "%8d", $1/1000 ) } END { printf( "\n" ); }'
+  $nodeview --readdb=$mf --summary --quiet | grep 'Total SUs' | cut -d: -f 2 | awk '{ printf( "%8d", $1 ) } END { printf( "\n" ); }'
 done
